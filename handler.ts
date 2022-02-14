b319cd4231313c859324903439c7ae9e031a1920
junaidkhan-aardy <junaid@aardy.com>
2022-02-14 17:08:27 +0000

    env

--- handler.ts
+++ handler.ts
@@ -1,11 +1,12 @@
 "use strict"
 
 module.exports.hello = async (event) => {
+  console.log("test", process.env.myStage)
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Go Serverless v3.0! Your function executed successfully@@@",
+        message: "Go Serverless v3.0! Your function executed successfullyprod",
       },
       null,
       2
